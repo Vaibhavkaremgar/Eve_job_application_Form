cb1f1becbdffb00c7a93bf6f50337cd21f85fc42
Sai Vignesh <suramsaivignesh@gmail.com>
2026-07-14 17:49:15 +0530

    Logs are removed and issues are resolved

--- app/auth.py
+++ app/auth.py
@@ -27,17 +27,17 @@ def create_session_cookie(email: str) -> str:
 def get_current_user(request: Request) -> str | None:
     token = request.cookies.get(SESSION_COOKIE)
 
-    print("Token:", token)
-    print("SESSION_SECRET:", os.environ.get("SESSION_SECRET"))
+    # print("Token:", token)
+    # print("SESSION_SECRET:", os.environ.get("SESSION_SECRET"))
 
     if not token:
         return None
     try:
         user =  _serializer.loads(token, max_age=SESSION_MAX_AGE)
-        print("Decoded User:", user)
+        # print("Decoded User:", user)
         return user
     except Exception as e:
-        print("Cookie Decode Error:", repr(e))
+        # print("Cookie Decode Error:", repr(e))
         return None
         
 
